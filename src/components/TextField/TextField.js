6b460ef7954eff78aedd5e520e4fef118e8a2fcb
GMorosini <gmorosini88@gmail.com>
2025-04-16 09:27:37 -0300

    TextField

---
+++ src/components/TextField/TextField.js
@@ -0,0 +1,12 @@
+import "./TextField.css"
+
+function TextField(){
+    return( 
+            <div className="TextField"> 
+                <label>Nome</label>
+                <input />
+            </div>
+          );
+}
+
+export default TextField;
